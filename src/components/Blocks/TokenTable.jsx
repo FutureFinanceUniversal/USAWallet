a42824b973d71a5fea9e901cc03b84662180bfcd
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2021-06-26 07:03:29 -0700

    Basic Chakra Table

--- src/components/Blocks/TokenTable.jsx
+++ src/components/Blocks/TokenTable.jsx
@@ -1,24 +1,49 @@
-import { Container, Text } from "@chakra-ui/react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react";
 import { useMoralis } from "react-moralis";
 import { useEffect, useState } from "react";
 
 export const TokenTable = () => {
   const { isAuthenticated, Moralis } = useMoralis();
-  const [dataArray, setDataArray] = useState({});
+  const [array, setData] = useState([]);
 
   useEffect(() => {
     if (isAuthenticated) {
-      Moralis.Web3.getAllERC20()
-        .then((dataArray) => {
-          setDataArray(dataArray);
-        })
-        .then(console.log);
+      Moralis.Web3.getAllERC20({ usePost: true }).then((dataArray) => {
+        setData(dataArray);
+      });
     }
   }, [isAuthenticated, Moralis]);
 
+  //<Text>{dataArray.map(d=>(<li></li>))}</Text>
   return (
-    <Container>
-      <Text>{dataArray.toString()}</Text>
-    </Container>
+    <Table variant="simple">
+      <TableCaption>- Token Balances -</TableCaption>
+      <Thead>
+        <Tr>
+          <Th>Token</Th>
+          <Th>Symbol</Th>
+          <Th isNumeric>Balance</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {array.map((r) => (
+          <Tr key={r.name}>
+            <Td>{r.name}</Td>
+            <Td>{r.symbol}</Td>
+            <Td>{r.balance / 10 ** r.decimals}</Td>
+          </Tr>
+        ))}
+      </Tbody>
+      <Tfoot></Tfoot>
+    </Table>
   );
 };
